34e12d9c5b9320d9ef863115ad06dbc36cd6f53b
ValentinaGottelli <valugottelli@gmial.com>
2019-10-21 17:38:18 -0300

    Proyecto

---
+++ login (copia)/registrocopia.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lan="es-AR"> 
+
+	<head>
+    <?php require_once "scripts.php"; ?>
+		<meta charset="utf-8">
+		<!-- Responsive -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title> Meta Gaming | Login up </title>
+		<!-- Favicon -->
+		<link rel="shortcut icon" type="image/icon" href="Imagenes/icos/favicon_2.jpg">
+
+		<!-- Enlace de bootstrap -->
+		<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<style>
+				padding-top: 56px;
+			
+		</style>
+	</head>
+	<body>	
+		<link rel="stylesheet" type="text/css" href="style.css">
+		    <!-- Start WOWSlider.com HEAD section -->
+		<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+		<script type="text/javascript" src="engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+<!-- Menú -->
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+			<div class="container">
+
+			  <a class="navbar-brand" href="index.html">Meta Gaming</a>
+			  <!-- Aqui se determina la caracteristica responsive del menu -->
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+			    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+					    <ul class="navbar-nav ml-auto">
+
+					      <!-- Primer elemento -->
+					      <li class="nav-item active">
+					        <a class="nav-link" href="index.html">Home <span class="sr-only"></span></a>
+					      </li>
+
+						  <!-- Segundo Elemento -->
+					      <li class="nav-item">
+					        <a class="nav-link" href="productos.html">Productos</a>
+
+					      </li>
+
+
+
+                          <!-- Tercer Elemento, Desplegable -->
+					      <li class="nav-item dropdown">
+					        <a class="nav-link" href="armar.html">Armar</a>
+
+					        <!-- Cuarto elemento -->
+					      <li class="nav-item dropdown">
+					        <a class="nav-link" href="contacto.html">Contacto</a>
+					         
+					       <!-- Tercer Elemento, Desplegable -->
+					      <li class="nav-item dropdown">
+					        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ingresar</a>
+					      <!-- Elementos Desplegables -->
+					        	<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					          		<a class="dropdown-item" href="acceder.html">Login</a>
+					          		<a class="dropdown-item" href="crear_cuenta.html">Registrarse</a>
+					      </li>
+
+					      <!-- Cuarto elemento -->
+					      <li class="nav-item dropdown">
+					        <a class="nav-link" href="">Nombre de Usuario</a>
+					       
+					    </ul>
+				</div>
+			</div>
+				</nav>
+
+
+
+
+				      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+				        <ol class="carousel-indicators">
+				          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				        </ol>
+				        <div class="carousel-inner" role="listbox">
+				          <!-- Slide One - Set the background image for this slide in the line below -->
+				          <div class="carousel-item active" style="background-image: Pantera.jpg">
+				            <div class="carousel-caption d-none d-md-block">
+				              <h3>First Slide</h3>
+				              <p>This is a description for the first slide.</p>
+				            </div>
+				          </div>
+				          <!-- Slide Two - Set the background image for this slide in the line below -->
+				          <div class="carousel-item" style="background-image: Pantera.jpg">
+				            <div class="carousel-caption d-none d-md-block">
+				              <h3>Second Slide</h3>
+				              <p>This is a description for the second slide.</p>
+				            </div>
+				          </div>
+				          <!-- Slide Three - Set the background image for this slide in the line below -->
+				          <div class="carousel-item" style="background-image: Pantera.jpg">
+				            <div class="carousel-caption d-none d-md-block">
+				              <h3>Third Slide</h3>
+				              <p>This is a description for the third slide.</p>
+				            </div>
+				          </div>
+				        </div>
+				        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				          <span class="sr-only">Previous</span>
+				        </a>
+				        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				          <span class="sr-only">Next</span>
+				        </a>
+				      </div>
+
+				      <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="well well-sm">
+                <form class="form-horizontal" method="post">
+                    <fieldset>
+                        <legend class="text-center header">Registrarse</legend>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control input-sm" id="fname" name="nombre" type="text" placeholder="Nombre" class="form-control">
+                            </div>
+                             <div class="col-md-8">
+                                <input type="text" class="form-control input-sm" id="fapellido" name="apellido" type="text" placeholder="Apellido" class="form-control">
+                            </div>
+                             <div class="col-md-8">
+                                <input type="text" class="form-control input-sm" id="fuser" name="user" type="text" placeholder="Usuario" class="form-control">
+                            </div>
+                             <div class="col-md-8">
+                                <input type="text" class="form-control input-sm" id="fdni" name="dni" type="text" placeholder="DNI" class="form-control">
+                            </div>
+                             <div class="col-md-8">
+                                <input type="text" class="form-control input-sm" id="fdireccion" name="direccion" type="text" placeholder="Direccion" class="form-control">
+                            </div>
+                        <div class="col-md-8">
+                            	<input type="text" class="form-control input-sm" type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+      						<div class="checkbox mb-3">
+                            </div>
+                        
+                        
+
+                        <div class="form-group">
+                            <div class="col-md-12 text-center">
+                                <button type="submit" class="btn btn-primary btn-lg" id="registrarNuevo" >Crear Cuenta</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+				
+
+
+ <!-- Pie de Página -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Meta Gaming 2018</p>
+
+
+		  <tr>
+		    <th> <a target="_blank" href="https://facebook.com"><img src="Imagenes/icos/Facebook.ico" border="0" width="50" height="50" alt="Facebook"></img></a> </th>
+		    <th> <a target="_blank" href="https://twitter.com/BillGates" style="right:10;"><img src="Imagenes/icos/Twitter.ico" border="0" width="50" height="50" alt="Twitter"></img></a></th>
+		    <th> <a target="_blank" href="https://instagram.com"><img src="Imagenes/icos/instagram.ico" border="0" width="50" height="50" alt="instagram"></img></a> </th>
+
+
+		  </tr>
+		</table>
+
+      </div>
+      <!-- /.container -->
+
+      </div>
+      <!-- /.container -->
+    </footer>
+
+		<!-- Enlace de JavaScript para asi usar todas las funcionalidades de las clases de Bootstrap -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	</body>
+</html>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#registrarNuevo').click(function(){
+
+			if($('#fname').val()==""){
+				alertify.alert("Debes agregar el nombre");
+				return false;
+			}else if($('#fapellido').val()==""){
+				alertify.alert("Debes agregar el apellido");
+				return false;
+			}else if($('#fuser').val()==""){
+				alertify.alert("Debes agregar el usuario");
+				return false;
+			}else if($('#fdni').val()==""){
+				alertify.alert("Debes agregar el dni");
+				return false;
+			}else if($('#fdireccion').val()==""){
+				alertify.alert("Debes agregar la direccion");
+				return false;
+			}else if($('#inputPassword').val()==""){
+				alertify.alert("Debes agregar el dni");
+				return false;
+			}
+
+			cadena="fname=" + $('#fname').val() +
+					"&fapellido=" + $('#fapellido').val() +
+					"&fuser=" + $('#fuser').val() + 
+					"&fdni=" + $('#fdni').val()+
+                    "&fdireccion="+ $('#fdireccion').val()+
+                    "&inputPassword="+ $('#inputPassword').val();
+
+					$.ajax({
+						type:"POST",
+						url:"php/registrocopia.php",
+						data:cadena,
+						success:function(r){
+
+							if(r==2){
+								alertify.alert("Este usuario ya existe, prueba con otro :)");
+							}
+							else if(r==1){
+								$('#frmRegistro')[0].reset();
+								alertify.success("Agregado con exito");
+							}else{
+								alertify.error("Fallo al agregar");
+							}
+						}
+					});
+		});
+	});
+
+</script>
